35439c08e66add10af2618db421425b76a02871d
Ivan Stasiuk <brokeyourbike@gmail.com>
2024-10-29 21:20:06 +0000

    test: status

--- tests/CashinStatusTest.php
+++ tests/CashinStatusTest.php
@@ -10,20 +10,18 @@ use Psr\Http\Message\ResponseInterface;
 use BrokeYourBike\IntouchGroup\Responses\CashinResponse;
 use BrokeYourBike\IntouchGroup\Interfaces\ConfigInterface;
 use BrokeYourBike\IntouchGroup\Interfaces\CashinInterface;
-use BrokeYourBike\IntouchGroup\Enums\ServiceEnum;
 use BrokeYourBike\IntouchGroup\Enums\CashinStatusEnum;
 use BrokeYourBike\IntouchGroup\Client;
 
 /**
  * @author Ivan Stasiuk <ivan@stasi.uk>
  */
-class CashinTest extends TestCase
+class CashinStatusTest extends TestCase
 {
     /** @test */
     public function it_can_prepare_request(): void
     {
         $transaction = $this->getMockBuilder(CashinInterface::class)->getMock();
-        $transaction->method('getServiceId')->willReturn(ServiceEnum::CASHIN_MTN);
 
         /** @var CashinInterface $transaction */
         $this->assertInstanceOf(CashinInterface::class, $transaction);
@@ -34,15 +32,7 @@ class CashinTest extends TestCase
         $mockedResponse = $this->getMockBuilder(ResponseInterface::class)->getMock();
         $mockedResponse->method('getStatusCode')->willReturn(200);
         $mockedResponse->method('getBody')
-            ->willReturn('  {
-                "service_id": "CODE SU SERVICE",
-                "gu_transaction_id": "1499360089225",
-                "status": "PENDING",
-                "transaction_date": "2017/07/06 16:54:49 PM",
-                "recipient_phone_number": "XXXXXXXXX",
-                "amount": 100,
-                "partner_transaction_id": "15908685691625"
-            }');
+            ->willReturn('{"status":"NOTFOUND","description":"No operation/transaction found for this PartnerNum and PartnerId"}');
 
         /** @var \Mockery\MockInterface $mockedClient */
         $mockedClient = \Mockery::mock(\GuzzleHttp\Client::class);
@@ -54,8 +44,8 @@ class CashinTest extends TestCase
          * */
         $api = new Client($mockedConfig, $mockedClient);
 
-        $requestResult = $api->cashin($transaction);
+        $requestResult = $api->status('15908685691625');
         $this->assertInstanceOf(CashinResponse::class, $requestResult);
-        $this->assertEquals(CashinStatusEnum::PENDING->value, $requestResult->status);
+        $this->assertEquals(CashinStatusEnum::NOTFOUND->value, $requestResult->status);
     }
 }
